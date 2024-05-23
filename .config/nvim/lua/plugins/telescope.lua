d80c79deb49338f277b24c03b59263bf0f62e4d9
poa <poaetcethe2nd@gmail.com>
2024-05-23 16:39:01 +0900

    changed telescope icons

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,4 +1,13 @@
+local opts = {
+	defaults = {
+		prompt_prefix = '',
+		selection_caret = '',
+	},
+}
+
 function config()
+	require('telescope').setup(opts)
+
 	local builtin = require('telescope.builtin')
 	vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 	vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
