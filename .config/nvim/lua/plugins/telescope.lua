9ad4e715745f3a92ecdad3590b088484798cc5c1
poa <poaetcethe2nd@gmail.com>
2024-05-31 01:20:10 +0900

    updated keymap parameters

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -20,8 +20,8 @@ return {
 		require('telescope').setup(opts)
 
 		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-		vim.keymap.set('n', '<leader>ft', builtin.treesitter, {})
+		vim.keymap.set('n', '<leader>ff', builtin.find_files)
+		vim.keymap.set('n', '<leader>fg', builtin.live_grep)
+		vim.keymap.set('n', '<leader>ft', builtin.treesitter)
 	end,
 }
